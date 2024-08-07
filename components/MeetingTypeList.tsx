13ddf3d9e2b627a3bc5c984fcaad7c652a6ab898
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-08-07 16:10:26 +0530

    feat: Implement reusable modal component

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,27 +4,32 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
 
 const MeetingTypeList = () => {
 
     const router = useRouter();
 
-    const [meetingState, setmeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>(undefined)
  
+    const createMeeting = () => {
+
+    }
+
  return (
    <section className="grid grid-cols-1 gap-5 mg:grid-cols-2 xl:grid-cols-4">
      <HomeCard
        img="/icons/add-meeting.svg"
        title="New Meeting"
        description="Start an instant meeting"
-       handleClick={() => setmeetingState("isJoiningMeeting")}
+       handleClick={() => setMeetingState("isInstantMeeting")}
        className="bg-orange-1"
      />
      <HomeCard
        img="/icons/schedule.svg"
        title="Schedule Meeting"
        description="Plan your meeting"
-       handleClick={() => setmeetingState("isScheduleMeeting")}
+       handleClick={() => setMeetingState("isScheduleMeeting")}
        className="bg-blue-1"
      />
      <HomeCard
@@ -38,8 +43,16 @@ const MeetingTypeList = () => {
        img="/icons/join-meeting.svg"
        title="Join Meeting"
        description="Via invitation Link"
-       handleClick={() => setmeetingState("isJoiningMeeting")}
+       handleClick={() => setMeetingState("isJoiningMeeting")}
        className="bg-yellow-1"
+     />
+     <MeetingModal 
+      isOpen={meetingState === 'isInstantMeeting'}
+      onClose={() => setMeetingState(undefined)}
+      title="State an Instant Meeting"
+      className="text-center"
+      buttonText="Start Meeting"
+      handleClick={createMeeting}
      />
    </section>
  );
